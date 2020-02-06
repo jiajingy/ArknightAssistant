882be8fa339349aa881097c27331f3ec91844f87
Xinwei Yu <jiajingy@outlook.com>
2020-02-05 20:08:23 -0500

    first commit

---
+++ Arknight/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Arknight.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Arknight
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RecruitView_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new RecruitViewModel();
+        }
+    }
+}
